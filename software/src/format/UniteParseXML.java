a188d797e17bb69e94bd04b7a7a68f7b9c1b42ae
151250214 <151250214@smail.nju.edu.cn>
2018-06-11 23:13:49 +0800

    统一格式的读取

---
+++ software/src/format/UniteParseXML.java
@@ -0,0 +1,85 @@
+package format;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import po.Course;
+import po.Selection;
+import po.Student;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by 朱应山 on 2018/6/11.
+ */
+public class UniteParseXML {
+
+
+    public ArrayList<Course> paserUniteCourse(String xmlStr) {
+        ArrayList<Course> list = new ArrayList<>();
+        try {
+            Document document = DocumentHelper.parseText(xmlStr);
+            Element Courses = document.getRootElement();
+            for (Iterator i = Courses.elementIterator(); i.hasNext(); ) {
+                Element element = (Element) i.next();
+                String courseID = element.elementText("课程编号");
+                String courseName = element.elementText("课程名称");
+                String teacher = element.elementText("授课老师");
+                String share = element.elementText("共享");
+                System.out.print(courseName);
+                Course course = new Course(courseID, courseName, teacher, share.charAt(0));
+                list.add(course);
+            }
+        } catch (DocumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+
+
+    public ArrayList<Student> paserUniteStudent(String xmlStr) {
+        ArrayList<Student> list = new ArrayList<>();
+        try {
+            Document document = DocumentHelper.parseText(xmlStr);
+            Element Students = document.getRootElement();
+            for (Iterator i = Students.elementIterator(); i.hasNext(); ) {
+                Element element = (Element) i.next();
+                String studentID = element.elementText("学号");
+                String Name = element.elementText("姓名");
+                String major = element.elementText("院系");
+                Student student = new Student(studentID, Name, major);
+                list.add(student);
+            }
+
+        } catch (DocumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+
+
+    public ArrayList<Selection> paserUniteSelection(String xmlStr) {
+        ArrayList<Selection> list = new ArrayList<>();
+        try {
+            Document document = DocumentHelper.parseText(xmlStr);
+            Element Selections = document.getRootElement();
+            for (Iterator i = Selections.elementIterator(); i.hasNext(); ) {
+                Element element = (Element) i.next();
+                String studentId = element.elementText("课程编号");
+                String courseID = element.elementText("学生编号");
+                String grade = element.elementText("成绩");
+
+                Selection selection = new Selection(studentId, courseID, Double.parseDouble(grade));
+                list.add(selection);
+            }
+
+        } catch (DocumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+
+
+}
